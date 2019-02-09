7c862188d07d0e91390217465cbd6d35966db379
Alex Gushulak <alex.gushulak@gmail.com>
2019-02-09 14:23:41 -0500

    Fixed folder structure for style

--- wimbeldon_serve/main.py
+++ wimbeldon_serve/main.py
@@ -4,8 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def my_form():
     return render_template('homepage.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    number_assignment = request.form['number_assign']
-    return number_assignment
