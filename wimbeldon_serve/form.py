bf11566917f018effe9be785e21b15874fd48e32
Alex Gushulak <alex.gushulak@gmail.com>
2019-02-09 15:12:49 -0500

    PROGRAMMING ELITE ELITE

--- wimbeldon_serve/form.py
+++ wimbeldon_serve/form.py
@@ -1,1 +1,7 @@
-from flask
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
--- wimbeldon_serve/main.py
+++ wimbeldon_serve/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template
+from form import RegistrationForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'asdfjsd824923'
 
 @app.route('/')
 def my_form():
-    return render_template('homepage.html')
+    form = RegistrationForm()
+    return render_template('homepage.html', title="Register", form=form)
