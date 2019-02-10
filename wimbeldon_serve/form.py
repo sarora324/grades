8d227a15fe48585f7e8ea782cf9ced47e63adc89
sarora324 <47468313+sarora324@users.noreply.github.com>
2019-02-09 19:17:03 -0500

    sup

--- wimbeldon_serve/form.py
+++ wimbeldon_serve/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.fields import IntegerField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+class EntryCategories(FlaskForm):
+    category = SelectField('category', choices=[('Exams','Exams'),('Quizzes','Quizzes'),('HW','HW'),('Projects','Projects')])
+    percentage = IntegerField('percentage', validators=[DataRequired(), NumberRange(min=0, max=100)])
+    numberItems = IntegerField('numberitems', validators=[DataRequired(), NumberRange(min=0, max=50)])
