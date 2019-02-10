6f515ad4e6b52366f1c0466185dba5020186e058
Alex Gushulak <alex.gushulak@gmail.com>
2019-02-09 19:16:02 -0500

    JQuery My God

--- wimbeldon_serve/form.py
+++ wimbeldon_serve/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+
+    submit = SubmitField('Calculate')
